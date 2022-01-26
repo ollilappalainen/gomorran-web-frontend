18c25fddf5fe085cb07049460377f37fa733b95f
Olli Lappalainen <olli@lamia.fi>
2022-01-26 13:04:19 +0200

    Initial commit

---
+++ components/Hero/Hero.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <img src="gomorran-logo.png" alt="Gomorran band logo" />
+        <img src="gomorran-band-photo-1.jpg" alt="Black and white Gomorran band photo" />
+    </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="root">
+        <header>
+
+        </header>
+        <main>
+            <Hero />
+        </main>
+        <footer>
+
+        </footer>
+    </div>
+</template>
+
+<script>
+import Hero from '~/components/Hero/Hero';
+
+export default {
+  name: 'IndexPage',
+  components: {
+      Hero,
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.root {
+    background-color: black;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+}
+</style>
