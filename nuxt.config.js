1d7a66fb471fd1f5c4050c54679cd65c93f494f3
Olli Lappalainen <olli@lamia.fi>
2022-01-30 00:17:52 +0200

    Fix font face

--- nuxt.config.js
+++ nuxt.config.js
@@ -22,7 +22,8 @@ export default {
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
       '@/assets/scss/colors.scss',
-      '@/assets/scss/defaults.scss'
+      '@/assets/scss/defaults.scss',
+      'material-design-icons-iconfont/dist/material-design-icons.css',
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
